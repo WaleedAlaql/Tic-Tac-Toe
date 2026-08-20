f8f26cb70d093f8e5d2a385e6a365b5d32e619de
Waleed Alaql <waleedalaql10@gmail.com>
2026-08-20 13:36:35 +0300

    implement board display and reset utility methods

--- src/main/java/com/waleed/TicTacToe.java
+++ src/main/java/com/waleed/TicTacToe.java
@@ -16,4 +16,30 @@ public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("=== Welcome to Tic Tac Toe! ===");
     }
+
+    /*
+    Reset the game board back to initial state
+     */
+    public static void resetBoard() {
+        char count = '1';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = count++;
+            }
+        }
+    }
+
+    /*
+    Display the current state of the game board
+     */
+    public static void printBoard() {
+        System.out.println("\n-------------");
+        for (int i = 0; i < 3; i++ ) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++ ) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
 }
